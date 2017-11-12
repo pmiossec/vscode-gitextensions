a1fe7d1a2814d308c1c207f9fbc13401a2f7c27e
Philippe Miossec <pmiossec@gmail.com>
2017-11-12 17:46:15 +0100

    Add support for multiple folders in a workspace

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,33 @@ import * as vscode from "vscode";
 import * as child from "child_process";
 import { window } from "vscode";
 
+const findWorkspaceFolder = () => {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+
+  if (workspaceFolders.length == 0) {
+    return null;
+  }
+  if (workspaceFolders.length == 1) {
+    return workspaceFolders[0].uri.fsPath;
+  }
+  if(window.activeTextEditor === undefined) {
+    return workspaceFolders[0].uri.fsPath;
+  }
+  const currentDocument = window.activeTextEditor.document.uri.fsPath;
+  const folder = vscode.workspace.workspaceFolders.find((w) => currentDocument.startsWith(w.uri.fsPath));
+  console.log('folder', folder);
+  return folder.uri.fsPath;
+}
+
 const launchGitExtensions = (command: string, args: string = '') => {
-  const path: string = vscode.workspace.workspaceFolders[0].uri.fsPath;
+  console.log(vscode.workspace.workspaceFolders);
+  const path: string = findWorkspaceFolder();
+  if(path === null)
+  {
+    console.error('No current folder found!');
+    return;
+  }
+
   child.exec(`gitExtensions ${command} ${args}`, { cwd: path} , (err, stdout, stderr) => {
     if (stdout) {
       console.log("[GitExtensions] stdout: " + stdout);
@@ -23,8 +48,7 @@ const launchGitExtensions = (command: string, args: string = '') => {
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Activing "vscode-gitextensions" extension.'
-  );
+  console.log('Activing "vscode-gitextensions" extension.');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
@@ -69,22 +93,27 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.gitextensions.difftool", () => {
-      const path = window.activeTextEditor.document.uri.fsPath;
-      launchGitExtensions("difftool", path);
+      if (window.activeTextEditor === undefined) {
+        return;
+      }
+      const filePath = window.activeTextEditor.document.uri.fsPath;
+      launchGitExtensions("difftool", filePath);
     })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.gitextensions.filehistory", () => {
-      const path = window.activeTextEditor.document.uri.fsPath;
-      launchGitExtensions("filehistory", path);
+      if (window.activeTextEditor === undefined) {
+        return;
+      }
+      const filePath = window.activeTextEditor.document.uri.fsPath;
+      launchGitExtensions("filehistory", filePath);
     })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.gitextensions.init", () => {
-      const path = vscode.workspace.workspaceFolders[0].uri.fsPath;
-      launchGitExtensions("init", path);
+      launchGitExtensions("init", findWorkspaceFolder());
     })
   );
 
