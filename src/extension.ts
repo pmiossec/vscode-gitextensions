8f1326ffdf52005d21bbb62b419c07cf3e8c11e6
Kevin Kahl <kevin.h.kahl+github@gmail.com>
2022-12-01 18:31:02 -0800

    Remove unnecessary console log output

--- src/extension.ts
+++ src/extension.ts
@@ -75,7 +75,7 @@ const registerCommandOnFile = (context: vscode.ExtensionContext, gitExtensionsPa
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Activing "vscode-gitextensions" extension.');
+  // console.log('Activing "vscode-gitextensions" extension.');
 
   const config = vscode.workspace.getConfiguration();
   const gitExtensionsPath = config.get<string>('gitExtensions.exe.path');
